088db081bcc510d0ccf75f82cfab803e7728dbb4
Jean Costa <jean.c.drawing@gmail.com>
2021-12-21 15:35:57 -0300

    Forth commit

---
+++ Autor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFBook
+{
+    public enum Sexo
+    {
+        Masculino = 1,
+        Feminino,
+        Outro
+    }
+    public class Autor
+    {
+        public int Id;
+        public string Nome;
+        public Sexo SexoAutor;
+        public List<Livro> Livros = new List<Livro>();
+
+        public bool Cadastrar(ref List<Autor> lista)
+        {
+            lista.Add(this);
+            return true;
+        }
+    }
+}
---
+++ Livro.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFBook
+{
+    public enum Genero
+    {
+        Comedia = 1,
+        Romance,
+        Drama,
+        Terror,
+        Suspense,
+        Aventura,
+        FicCientifica
+
+    }
+    public class Livro
+    {
+        public int Id; 
+        public Autor NomeAutor; 
+        public int Paginas;
+        public string Editora;
+        public string Titulo;
+        public Genero GeneroLivro; 
+        public int Ano;
+
+        public Autor ProcurarAutor(string nome)
+        {
+            foreach (Autor a in FormPrincipal.Autores)
+            {
+                if (a.Nome.Contains(nome) == true)
+                    return a;
+            }
+            return null;
+        }
+
+        public bool Cadastrar(ref List<Livro> livros)
+        {
+            livros.Add(this);
+            return true;
+        }
+    }
+}
